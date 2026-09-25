81949aba9b253480a9b08a70eb0251f4960f57c1
Jan Kubat <cristianobenucci5@gmail.com>
2026-09-25 18:53:41 +0200

    skladowe statyczne

--- Apteka/Lek.cs
+++ Apteka/Lek.cs
@@ -2,6 +2,7 @@
 
 public class Lek
 {
+    public static int LiczbaLekow { get; private set; } = 0;
     public string Nazwa { get; set; }
     public decimal Cena { get; set; }
     public int IloscNaStanie { get; set; }
@@ -11,10 +12,26 @@ public class Lek
         Nazwa = nazwa;
         Cena = cena;
         IloscNaStanie = ilosc;
+        LiczbaLekow++;
     }
 
     public override string ToString()
     {
         return $"Lek: {Nazwa}, Cena: {Cena}, Ilość na stanie: {IloscNaStanie}";
+    }
+
+    public bool CzyDostepny()
+    {
+        return IloscNaStanie > 0;
+    }
+
+    public void Sprzedaj(int ilosc)
+    {
+        IloscNaStanie -= ilosc;
+    }
+    
+    public static void WypiszLiczbeLekow()
+    {
+        Console.WriteLine($"Mamy {LiczbaLekow} lekow w aptece.");
     }
 }
--- Apteka/Program.cs
+++ Apteka/Program.cs
@@ -7,3 +7,12 @@ Console.WriteLine(z);
     
 var lek = new Lek("Paracetamol", 59.99m, 10);
 Console.WriteLine(lek);
+Console.WriteLine(lek.CzyDostepny());
+
+lek.Sprzedaj(4);
+Console.WriteLine(lek);
+
+var lek2 = new Lek("Ibuprom", 24.50m, 0);
+Console.WriteLine(lek2.CzyDostepny());
+
+Lek.WypiszLiczbeLekow();
