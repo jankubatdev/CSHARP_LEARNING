2012b185c37044d6fa4b78bc1aea926bd5c032a1
jankubatdev <138157662+jankubatdev@users.noreply.github.com>
2026-09-25 11:41:28 +0200

    build apteka

---
+++ Apteka/Lek.cs
@@ -0,0 +1,20 @@
+﻿namespace Apteka;
+
+public class Lek
+{
+    public string Nazwa { get; set; }
+    public decimal Cena { get; set; }
+    public int IloscNaStanie { get; set; }
+
+    public Lek(string nazwa, decimal cena, int ilosc)
+    {
+        Nazwa = nazwa;
+        Cena = cena;
+        IloscNaStanie = ilosc;
+    }
+
+    public override string ToString()
+    {
+        return $"Lek: {Nazwa}, Cena: {Cena}, Ilość na stanie: {IloscNaStanie}";
+    }
+}
